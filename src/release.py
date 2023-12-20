1d9e66dc6f24063400f6e4fe90ea2426d496f0ea
Mryan2005 <A2564011261@163.com>
2023-12-20 08:21:39 +0800

    update

--- src/release.py
+++ src/release.py
@@ -4,16 +4,23 @@ import settings
 # 读取配置文件
 user = settings.user
 # import Edge的Service
-from selenium.webdriver.edge.service import Service
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
+from pyvirtualdisplay import Display
 from selenium.webdriver.common.by import By
 import json
 import sys
 import requests
 def release(Url, Username, Password, title, content):
+    #display = Display(visible=0, size=(1280, 768))
+    #display.start()
     ser = Service()
     ser.path = 'C:\\Users\\A2564\\AppData\\Local\\Programs\\Python\\Python311\\geckodriver.exe'
     # 连接Edge浏览器
-    driver = webdriver.Firefox(service=ser)
+    firefox_options = Options()
+    firefox_options.headless = True
+    driver = webdriver.Firefox(options=firefox_options, service=ser)
     driver.get(Url)
     loginTag = driver.find_element(By.CLASS_NAME, "item-logIn")
     loginTag.click()
@@ -38,5 +45,5 @@ def release(Url, Username, Password, title, content):
     suubmitTag = driver.find_element(By.XPATH, "//div[@class=\"TagSelectionModal-form-submit App-primaryControl\"]")
     suubmitTag.click()
     driver.close()
-if __name__ == "__main__":
-    release(user["url"], user["username"], user["password"], "test", "test")
+if __name__ == "__main__":  
+    release(sys.argv[1], sys.argv[2], sys.argv[3], "test", "test")
