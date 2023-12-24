f5cc215b0104390fedb279fa5d5070bd600f4cc1
Mryan2005 <A2564011261@163.com>
2023-12-24 13:46:59 +0800

    fix(main.py): fix module can't find

--- SSPUBot/main.py
+++ SSPUBot/main.py
@@ -1,6 +1,11 @@
-from getInformation import getInformation as g
-from release import release as r
-from settings import settings as s
+try:
+    from getInformation import getInformation as g
+    from release import release as r
+    from settings import settings as s
+except ModuleNotFoundError:
+    from SSPUBot.getInformation import getInformation as g
+    from SSPUBot.release import release as r
+    from SSPUBot.settings import settings as s
 def run():
     g.get()
     file = open("result.md", "r")
