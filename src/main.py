7505ded92fcd2f0dfeb87c6ceb2c261305eb0fc6
Mryan2005 <A2564011261@163.com>
2023-12-24 13:19:30 +0800

    fix(main.py): create a function "run"

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 from getInformation import *
 from release import *
 from settings import *
-get()
-file = open("result.md", "r")
-content = file.read()
-file.close()
-release(user["url"], user["username"], user["password"], "test", content)
+def run():
+    get()
+    file = open("result.md", "r")
+    content = file.read()
+    file.close()
+    release(user["url"], user["username"], user["password"], "test", content)
+if __name__ == "__main__":
+    run()
