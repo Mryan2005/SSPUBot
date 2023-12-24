5320b4c7bce2c7a458dd07586be9ee41fa05dd0a
Mryan2005 <A2564011261@163.com>
2023-12-24 15:03:59 +0800

    add(main.py): It can check if result.md have notices

--- SSPUBot/main.py
+++ SSPUBot/main.py
@@ -1,3 +1,6 @@
+from itertools import count
+from sys import flags
+import sys
 try:
     from getInformation import getInformation as g
     from release import release as r
@@ -7,10 +10,41 @@ except ModuleNotFoundError:
     from SSPUBot.release import release as r
     from SSPUBot.settings import settings as s
 def run():
+    flag = 0
+    count = 0
+    noNotice = [0, 0, 0]
     g.get()
     file = open("result.md", "r")
-    content = file.read()
-    file.close()
-    r.release(s.user["url"], s.user["username"], s.user["password"], "test", content)
+    contents = file.readlines()
+    for i in contents:
+        if("## 教务处通知" == i and flag == 0):
+            flag = 1
+        if(flag == 1):
+            if("\n" == i):
+                flag = 2
+        if(flag == 2):
+            if("## 体育部通知" == i):
+                noNotice[0] = 1
+            flag = 3
+        if(flag == 3):
+            if("\n" == i):
+                flag = 4
+        if(flag == 4):
+            if("## 青春二工大" == i):
+                noNotice[1] = 1
+            flag = 5
+        if(flag == 5):
+            if("\n" == i):
+                flag = 6
+        if(flag == 6):
+            if("\n" == i):
+                noNotice[2] = 1
+                break
+    for i in noNotice:
+        if(i == 1):
+            count += 1
+    if(count != 3):
+        file.close()
+        r.release(s.user["url"], s.user["username"], s.user["password"], "test", content)
 if __name__ == "__main__":
     run()
