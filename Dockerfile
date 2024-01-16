13aafc6014e28fe172f24500e6f4dcff9b459a15
Zhizhong Yan <A2564011261@163.com>
2024-01-16 17:24:42 +0800

    add(workflows): add Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM ubuntu:latest
+LABEL authors="Mryan2005"
+RUN apt-get update && apt-get install -y python3.11 python3-pip firefox
+RUN pip install -r requirements.txt
+COPY ./Firefox/geckodriver /usr/bin/geckodriver
+CMD ["python3", "main.py"]
+ENTRYPOINT ["top", "-b"]
--- SSPUBot/release/release.py
+++ SSPUBot/release/release.py
@@ -4,12 +4,18 @@ import sys
 import time
 import json
 import requests
-
-s = json.load(open("./settings/settings.json", "r", encoding="utf-8"))
+import logging
+try:
+    s = json.load(open("./data/settings/settings.json", "r", encoding="utf-8"))
+except FileNotFoundError:
+    s = json.load(open("../data/settings/settings.json", "r", encoding="utf-8"))
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', filename="log.txt", filemode='a+')  # 日志配置
 
 
 def release(Url, token, title, outline, url, isTest):
     session = requests.Session()
+    logging.info("Release start!")
+    logging.info("正在尝试连接到服务器...")
     responses = session.get(Url)
     head = {
         "Accept": "application/vnd.api+json",
@@ -17,15 +23,17 @@ def release(Url, token, title, outline, url, isTest):
         "Authorization": "Token " + token
     }
     if isTest:
+        logging.info("现在是测试模式")
         id = 19
     else:
+        logging.info("现在是正式模式")
         id = 20
     data = {
         "data": {
             "type": "discussions",
             "attributes": {
                 "title": title,
-                "content": outline + "  \n[ 前往官网 ](" + url + ")"
+                "content": outline + "  \n[ 前往官网 ](" + str(url) + ")"
             },
             "relationships": {
                 "tags": {
@@ -42,6 +50,11 @@ def release(Url, token, title, outline, url, isTest):
     responses = session.post(Url + "/api/discussions", headers=head, json=data)
     if responses.status_code == 201:
         print("Release success!")
+        logging.info("Release"+ title + " " + str(url) + " success!")
+    else:
+        print("Release failed!")
+        logging.error("Release"+ title + " " + str(url) + " failed!")
+    logging.info("Release end!")
 
 
 if __name__ == "__main__":
