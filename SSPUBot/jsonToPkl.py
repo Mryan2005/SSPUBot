77824803e5e04f416ad9506e1bd37b328328e809
Zhizhong Yan <A2564011261@163.com>
2024-03-10 09:22:10 +0800

    add(main.py): add a function.

--- SSPUBot/jsonToPkl.py
+++ SSPUBot/jsonToPkl.py
@@ -1,5 +1,7 @@
 import json
 import pickle
 
-settings = json.load(open("data/settings/key.json"))
-pickle.dump(settings, open("data/settings/key.pkl", "wb"))
+
+def jsonToPkl():
+    settings = json.load(open("data/settings/key.json"))
+    pickle.dump(settings, open("data/settings/key.pkl", "wb"))
