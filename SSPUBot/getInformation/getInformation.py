2d28fccf49eeb08bf2cc8260c1a5a7343a62ce1d
Zhizhong Yan <A2564011261@163.com>
2024-05-03 15:23:48 +0800

    fix(getInformation.py): fix some things

--- SSPUBot/getInformation/getInformation.py
+++ SSPUBot/getInformation/getInformation.py
@@ -508,8 +508,13 @@ def getSchooljwc():
                 flag = 0
                 break
     logging.info("获取教务处文件的概要成功，正在获取教务处文件")
+    oldPosts = open("./data/haveReleased.sspubot", "r+", encoding="utf-8")
+    oldPostList = oldPosts.readlines()
+    oldPosts.close()
     for g in posts:
         url = g.url
+        if url + '\n' in oldPostList or title + '\n' in oldPostList:
+            continue
         try:
             driver.get(url)
             outline = driver.find_element(By.XPATH, "//div[@class=\"WordSection1\"]")
@@ -625,8 +630,13 @@ def getschoolpe():
                 break
         file.close()
     logging.info("获取体育部文件的概要成功，正在获取体育部文件")
+    oldPosts = open("./data/haveReleased.sspubot", "r+", encoding="utf-8")
+    oldPostList = oldPosts.readlines()
+    oldPosts.close()
     for g in posts[lastpart + 1:]:
         url = g.url
+        if url + '\n' in oldPostList or title + '\n' in oldPostList:
+            continue
         try:
             if "files/" in url:
                 g.setOutline("由于网页不支持打开，请到该站点查看")
